9d1ad4cc7efb94900c247bb977af3cd4b06b581a
Russell Ries <riesru@gmail.com>
2016-04-21 15:44:58 -0500

    finish exercise

--- numbers.js
+++ numbers.js
@@ -2,3 +2,21 @@
 // and the square of the sum of the first ten natural numbers?
 
 
+var natural = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var square = [];
+
+for (var i = 0; i < natural.length; i++) {
+  var double = natural[i] * natural[i];
+  square.push(double);
+}
+
+var naturalSum = natural.reduce(add, 0);
+var squareSum = square.reduce(add, 0);
+
+function add(a, b) {
+  return a + b;
+}
+
+var answer = squareSum - naturalSum;
+
+console.log("Answer: ", answer);
